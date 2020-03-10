0c610e662e6bc4a911934b0fa393f72a9252af83
Noel Delgado <auhcsei@gmail.com>
2020-03-09 19:49:17 -0600

    :recycle: update gulpfile format for gulp@4

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,54 @@
-var gulp            = require('gulp'),
-    sass            = require('gulp-ruby-sass'),
+var { src, dest, parallel, watch }= require('gulp'),
+    sass            = require('gulp-sass'),
     autoprefixer    = require('gulp-autoprefixer'),
     concat          = require('gulp-concat'),
-    uglify          = require('gulp-uglify')
+    uglify          = require('gulp-uglify-es').default,
     livereload      = require('gulp-livereload'),
     connect         = require('gulp-connect')
 
-gulp.task('connect', function() {
+const serverTask = (done) => {
     connect.server({
         root: '',
         livereload: true
     });
-});
+    done();
+}
 
-gulp.task('styles', function() {
-    return gulp.src('src/sass/app.scss')
-        .pipe(sass({
-            style: 'compressed'
-        }))
-        .pipe(autoprefixer('last 2 version'))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(connect.reload());
-});
+const stylesTask = () => src('src/sass/app.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(dest('dist/css'))
+    .pipe(connect.reload());
 
-gulp.task('scripts', function() {
-    return gulp.src([
-        'bower_components/neon/neon.js',
-        'bower_components/neon/stdlib/node_support.js',
-        'bower_components/neon/stdlib/custom_event.js',
-        'bower_components/neon/stdlib/custom_event_support.js',
-        'bower_components/neon/stdlib/node_support.js',
-        'bower_components/values.js/index.js',
-        'src/javascripts/lib/**',
-        'src/javascripts/ui/**',
-        'src/javascripts/app/**',
-        'src/javascripts/app.js'
-        ])
-        .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'))
-        .pipe(connect.reload());
-});
+const scriptsTask = () => src([
+    'node_modules/neon/neon.js',
+    'node_modules/neon/stdlib/**',
+    'node_modules/values.js/index.js',
+    'src/javascripts/lib/**',
+    'src/javascripts/ui/**',
+    'src/javascripts/app/**',
+    'src/javascripts/app.js'
+])
+    .pipe(concat('app.js'))
+    .pipe(dest('dist/js'))
+    .pipe(connect.reload());
 
-gulp.task('markup', function() {
-    return gulp.src('*.html')
-        .pipe(connect.reload());
-});
+const htmlTask = () => src('*.html').pipe(connect.reload());
 
-gulp.task('watch', ['connect'], function() {
-    gulp.watch('*.html', ['markup'])
-    gulp.watch('src/javascripts/**', ['scripts'])
-    gulp.watch('src/sass/**', ['styles'])
-});
+const watchTask = (done) => {
+    watch('*.html', htmlTask);
+    watch('src/javascripts/**', scriptsTask);
+    watch('src/sass/**', stylesTask);
+    done();
+}
 
-gulp.task('default', ['watch']);
+const stylesDist = () => src('dist/css/app.css')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(dest('dist/css'));
+
+const scriptsDist = () => src('dist/js/app.js')
+    .pipe(uglify())
+    .pipe(dest('dist/js/'));
+
+exports.default = parallel(watchTask, serverTask);
+exports.build = parallel(stylesDist, scriptsDist);
