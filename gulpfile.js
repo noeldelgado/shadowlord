5d30c24e1ba09c03970c2bd6e2196974b8ed2d49
Noel Delgado <auhcsei@gmail.com>
2020-04-29 22:09:36 -0600

    :wrench: update gulpfile to use rollup

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,76 @@
-var { src, dest, parallel, watch }= require('gulp'),
-    sass            = require('gulp-sass'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    concat          = require('gulp-concat'),
-    uglify          = require('gulp-uglify-es').default,
-    livereload      = require('gulp-livereload'),
-    connect         = require('gulp-connect')
+const { src, dest, parallel, watch } = require('gulp'),
+  rollup          = require('gulp-better-rollup'),
+  babel           = require('rollup-plugin-babel'),
+  sass            = require('gulp-sass'),
+  autoprefixer    = require('gulp-autoprefixer'),
+  concat          = require('gulp-concat'),
+  uglify          = require('gulp-uglify-es').default,
+  livereload      = require('gulp-livereload'),
+  connect         = require('gulp-connect'),
+  rootImport      = require('rollup-plugin-root-import'),
+  nodeResolve     = require('rollup-plugin-node-resolve'),
+  commonjs        = require('rollup-plugin-commonjs');
 
 sass.compiler = require('sass');
 
 const serverTask = (done) => {
-    connect.server({
-        root: '',
-        livereload: true
-    });
-    done();
+  connect.server({
+    root: '',
+    livereload: true
+  });
+  done();
 }
 
 const stylesTask = () => src('src/sass/app.scss')
-    .pipe(sass())
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(dest('dist/css'))
-    .pipe(connect.reload());
+  .pipe(sass({
+    includePaths: [
+      'node_modules',
+      require('path').resolve(__dirname, 'node_modules')
+    ]
+  }))
+  .pipe(autoprefixer('last 2 version'))
+  .pipe(dest('dist/css'))
+  .pipe(connect.reload());
 
-const scriptsTask = () => src([
-    'node_modules/neon/neon.js',
-    'node_modules/neon/stdlib/**',
-    'node_modules/values.js/index.js',
-    'src/javascripts/lib/**',
-    'src/javascripts/ui/**',
-    'src/javascripts/app/**',
-    'src/javascripts/app.js'
-])
-    .pipe(concat('app.js'))
-    .pipe(dest('dist/js'))
-    .pipe(connect.reload());
+const scriptsTask = () => src('src/javascripts/index.js')
+  .pipe(rollup({
+    // There is no `input` option as rollup integrates into the gulp pipeline
+    plugins: [
+      nodeResolve({
+        mainFields: ['module', 'main', 'browser']
+      }),
+      commonjs({
+        include: ['node_modules/**']
+      }),
+      rootImport({
+        root: `src/javascripts`,
+        extensions: '.js'
+      }),
+      babel()
+    ]
+  }, {
+    // Rollups `sourcemap` option is unsupported. Use `gulp-sourcemaps` plugin instead
+    format: 'umd',
+  }))
+  .pipe(dest('dist/js'))
+  .pipe(connect.reload());
 
 const htmlTask = () => src('*.html').pipe(connect.reload());
 
 const watchTask = (done) => {
-    watch('*.html', htmlTask);
-    watch('src/javascripts/**', scriptsTask);
-    watch('src/sass/**', stylesTask);
-    done();
+  watch('*.html', htmlTask);
+  watch('src/javascripts/**', scriptsTask);
+  watch('src/sass/**', stylesTask);
+  done();
 }
 
 const stylesDist = () => src('dist/css/app.css')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(dest('dist/css'));
+  .pipe(sass({ outputStyle: 'compressed' }))
+  .pipe(dest('dist/css'));
 
-const scriptsDist = () => src('dist/js/app.js')
-    .pipe(uglify())
-    .pipe(dest('dist/js/'));
+const scriptsDist = () => src('dist/js/index.js')
+  .pipe(uglify())
+  .pipe(dest('dist/js/'));
 
 exports.default = parallel(watchTask, serverTask);
 exports.build = parallel(stylesDist, scriptsDist);
