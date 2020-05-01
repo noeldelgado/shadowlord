4755ef05ee892c475e3858fd87d05bbaeda7f97c
Noel Delgado <auhcsei@gmail.com>
2020-05-01 16:13:02 -0600

    add mdc/SVGIcon

---
+++ src/javascripts/lib/mdc/SVGIcon.js
@@ -0,0 +1,19 @@
+import Widget from '/lib/widget/index';
+
+export default class SVGIcon extends Widget {
+
+  static defaults = {
+    viewBox: '0 0 24 24',
+    content: '',
+    width: '100%',
+    height: '100%'
+  }
+
+  template() {
+    return `
+    <svg viewBox='${this.viewBox}' width=${this.width} height=${this.height}>
+      ${this.content}
+    </svg>
+    `;
+  }
+}
